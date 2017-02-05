373039531e841e0f47ee168d6a55387c13157921
Fadly Kayo <f_kayo@yahoo.com>
2017-02-05 15:45:25 +0700

    Done refactor procedural to OO

--- assets/javascripts/index.js
+++ assets/javascripts/index.js
@@ -1,12 +1,38 @@
-$(document).ready(function () {
-  $('#roller button.add').on('click', function () {
+'use strict'
+
+const controller = {
+  add: function () {
     console.log('WAT')
     $('.dice').append('<div class="die">0</div>')
-  })
-  $('#roller button.roll').on('click', function () {
+  },
+  roll: function () {
     $('.die').each(function (k, die) {
-      var value = Math.floor((Math.random() * 6) + 1)
-      $(die).text(value)
+      $(die).text(model.randomVal())
     })
-  })
+  }
+}
+
+const model = {
+  randomVal: function () {
+    var value = Math.floor((Math.random() * 6) + 1)
+    return value
+  }
+}
+
+$(document).ready(function () {
+  $('#roller button.add').on('click', controller.add)
+  $('#roller button.roll').on('click', controller.roll)
 })
+
+// $(document).ready(function () {
+//   $('#roller button.add').on('click', function () {
+//     console.log('WAT')
+//     $('.dice').append('<div class="die">0</div>')
+//   })
+//   $('#roller button.roll').on('click', function () {
+//     $('.die').each(function (k, die) {
+//       var value = Math.floor((Math.random() * 6) + 1)
+//       $(die).text(value)
+//     })
+//   })
+// })
